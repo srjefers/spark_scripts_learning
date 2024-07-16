ea7c32d46c5e7c22c99a92b2d9d15b67293c254c
srjefers <ajefersonstiv@gmail.com>
2024-07-15 23:38:17 -0600

    Exercise 4 Solved

--- spark_sql/exercise_4.py
+++ spark_sql/exercise_4.py
@@ -1,12 +1,17 @@
 from pyspark.sql import SparkSession, HiveContext
-# from pyspark.sql.functions import withColumn
+from pyspark.sql.functions import col, collect_set, udf
+# collect_set
 
 spark = (SparkSession
          .builder
          .appName('exersice_4')
          .getOrCreate())
 
-df = spark.range(50).withColumn("key", "id" % 5)
+df = spark.range(50).withColumn("key", col("id") % 5)
 
-df.show()
-# df.registerTempTable("dfTable")
+split_row = udf(lambda row: row[:3])
+
+df2 = df.groupBy('key').agg(collect_set('id').alias('all'))
+df2 = df2.withColumn('only_first_three', split_row(col('all')))
+
+df2.show()
