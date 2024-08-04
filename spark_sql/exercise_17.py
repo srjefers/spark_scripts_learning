5892ed67c05be7def58aa8bdd11aaa9fc9cf57b2
srjefers <ajefersonstiv@gmail.com>
2024-08-03 22:01:38 -0600

    Exercise_17 Development

--- spark_sql/exercise_17.py
+++ spark_sql/exercise_17.py
@@ -1,19 +1,24 @@
 """
     url: https://jaceklaskowski.github.io/spark-workshop/exercises/spark-sql-exercise-Using-explode-Standard-Function.html
 """
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType,ArrayType, IntegerType
+from pyspark.sql.functions import col, explode
+
+sc = SparkContext("local", "Simple App")
 
 spark = (SparkSession
          .builder
          .appName('exersice_17')
          .getOrCreate())
+"""
+data = [1,2,3]
+rdd = sc.parallelize(data)
 
-nums = ArrayType([
-    StructField("elemnt", IntegerType(), nullable=False),
-])
+df = spark.createDataFrame(rdd.flatMap(lambda x: [[data, x]]).collect(),['nums','num'])
+"""
 
-df = spark.createDataFrame([[1,2,3]],['nums'],schema=MovieCriticsSchema)
-
-df.printSchema()
+df = spark.createDataFrame([[[1,2,3]]])
+df = df.select(col("_1").alias('nums'), explode(col("_1")).alias('num'))
 df.show()
