a5fbc38df935aa67258d7fa0abde50ffe51d2bec
srjefers <ajefersonstiv@gmail.com>
2024-08-13 00:43:20 -0600

    Models exported to csv

--- spark_sql/spark_pivot/pivot_script_1.py
+++ spark_sql/spark_pivot/pivot_script_1.py
@@ -13,4 +13,9 @@ df = spark.createDataFrame(pdf)
 df.show()
 
 unpivotExp = "stack(5, '2018', 2018, '2019', 2019, '2020', 2020, '2021', 2021, '2022', 2022) as (year, val)"
-df.select('State or territory','Type', 'Unnamed: 2',expr(unpivotExp)).show()
+df2 = df.select('State or territory','Type', 'Unnamed: 2',expr(unpivotExp))
+#df2.repartition(1).write.option("header", "true").csv('model_1.csv', mode = 'append')
+# .save('model_1.csv')
+
+df2.toPandas().to_csv('model_1.csv')
+
--- spark_sql/spark_pivot/pivot_script_4.py
+++ spark_sql/spark_pivot/pivot_script_4.py
@@ -10,4 +10,6 @@ pdf = pandas.read_excel(filePath, sheet_name='4')
 df = spark.createDataFrame(pdf)
 
 #df.show()
-df.unpivot(['Year','State/Territory','Sex','Age'],['Uncapped Participation Rate: Full-time and Part-time Students','Uncapped Participation Rate: Full-time Students'], 'type', 'val').show()
+df2 = df.unpivot(['Year','State/Territory','Sex','Age'],['Uncapped Participation Rate: Full-time and Part-time Students','Uncapped Participation Rate: Full-time Students'], 'type', 'val')
+df2.toPandas().to_csv('model_4.csv')
+
