afc0fb0e2c948e4830396df44b5db1d4af9b6cb8
srjefers <ajefersonstiv@gmail.com>
2024-08-13 00:00:53 -0600

    pivot script 2 solved

--- spark_sql/spark_pivot/pivot_script_1.py
+++ spark_sql/spark_pivot/pivot_script_1.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import first, col, posexplode, monotonically_increasing_id, expr
+from pyspark.sql.functions import col, monotonically_increasing_id, expr
 import pandas
 
-spark = SparkSession.builder.appName("Test").getOrCreate()
+spark = SparkSession.builder.appName("pivot_script_1").getOrCreate()
 
 filePath = "/home/kernelpanic/Documents/SPARK/spark_sql/spark_pivot/BaseDatos.xlsx"
 
@@ -14,5 +14,3 @@ df.show()
 
 unpivotExp = "stack(5, '2018', 2018, '2019', 2019, '2020', 2020, '2021', 2021, '2022', 2022) as (year, val)"
 df.select('State or territory','Type', 'Unnamed: 2',expr(unpivotExp)).show()
-
-
---
+++ spark_sql/spark_pivot/pivot_script_2.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, monotonically_increasing_id, expr
+import pandas
+
+spark = SparkSession.builder.appName("pivot_script_2").getOrCreate()
+
+filePath = "/home/kernelpanic/Documents/SPARK/spark_sql/spark_pivot/BaseDatos.xlsx"
+
+pdf = pandas.read_excel(filePath, sheet_name='2')
+columns = pdf.columns.tolist()
+columns.remove('Unnamed: 0')
+columns.remove('Unnamed: 1')
+for column in columns:
+    pdf[column] = pdf[column].astype(str)
+df = spark.createDataFrame(pdf)
+
+for column in columns:
+    df = df.withColumn(column.replace('–','-'), col(column))
+#df.show()
+
+unpivotExp = "stack(15,'2008-09',2008-09,'2009-10',2009-10,'2010-11',2010-11,'2011-12',2011-12,'2012-13',2012-13,'2013-14',2013-14,'2014-15',2014-15,'2015-16',2015-16,'2016-17',2016-17,'2017-18',2017-18,'2018-19',2018-19,'2019-20',2019-20,'2020-21',2020-21,'2021-22',2021-22,'2022-23',2022-23) as (year, val)"
+df.select('Unnamed: 0','Unnamed: 1',expr(unpivotExp)).show()
+
