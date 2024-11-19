59ac2b59fda6e2091d0f1afa95c815be9e504b08
Jeferson Argueta <ajefersonstiv@gmail.com>
2024-11-19 15:54:33 -0600

    Exercise_22 solved

--- spark_sql/exercise_22.py
+++ spark_sql/exercise_22.py
@@ -3,6 +3,8 @@
 """
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.types import StringType
 
 sc = SparkContext("local", "Simple App")
 
@@ -11,3 +13,19 @@ spark = (SparkSession
          .appName('exersice_22')
          .getOrCreate())
 
+df = spark.createDataFrame([
+    ("a"),
+    ("b"),
+    ("c"),
+    ("d"),
+    ("d")
+],StringType()).toDF("letter")
+
+def toUpperCase(text: str) -> str:
+    return text.upper()
+
+toUpperCaseUdf = udf(toUpperCase, StringType())
+
+df = df.withColumn("letterUpperCase", toUpperCaseUdf(col("letter")))
+
+df.show()
