e94b2b9644e762e9037a192ae26c0a805662886d
Jeferson Argueta <ajefersonstiv@gmail.com>
2024-11-25 20:04:16 -0600

    Exercise 27 Solved

--- spark_sql/exercise_27/exercise_27.py
+++ spark_sql/exercise_27/exercise_27.py
@@ -3,6 +3,7 @@
 """
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lit
 
 sc = SparkContext("local", "Simple App")
 
@@ -12,5 +13,7 @@ spark = (SparkSession
          .getOrCreate())
 
 df = spark.read.options(delimiter=',',inferSchema=True,header=True).csv('data.csv')
-
+#df.show()
+df = df.groupBy().pivot("udate",['udate','20090622','20090624','20090626','20090629','20090914']).sum("cc")
+df = df.withColumn("udate",lit('cc'))
 df.show()
