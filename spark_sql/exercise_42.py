a28a1b812df82b4e5ca1c12f5155715da6016ca1
Jeferson Argueta <ajefersonstiv@gmail.com>
2025-01-09 21:20:33 -0600

    Exercise 42 OnDEV

---
+++ spark_sql/exercise_42.py
@@ -0,0 +1,12 @@
+"""
+    url: https://jaceklaskowski.github.io/spark-workshop/exercises/spark-sql-exercise-Finding-Most-Common-Non-null-Prefix-Occurences-per-Group.html
+"""
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+sc = SparkContext("local", "Simple App")
+
+spark = (SparkSession
+         .builder
+         .appName('exersice_42')
+         .getOrCreate())
