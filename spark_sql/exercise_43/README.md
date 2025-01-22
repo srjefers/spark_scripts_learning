406fd2e9044cfc4caab44d5335367e401574bf69
Jeferson Argueta <ajefersonstiv@gmail.com>
2025-01-22 12:07:27 -0600

    Exercise 43 UnDone

---
+++ spark_sql/exercise_43/README.md
@@ -0,0 +1,9 @@
+## Issue
+I tried to implement a solution to read a custom data source but didn't has success while tried to use differnet functions from pyspark.
+
+So I am going to leave this for another time.
+
+## References
+* https://learn.microsoft.com/en-us/azure/databricks/pyspark/datasources
+* https://spark.apache.org/docs/4.0.0-preview1/api/python/user_guide/sql/python_data_source.html
+* https://spark.apache.org/docs/4.0.0-preview2/api/python/_modules/pyspark/sql/datasource.html
--- spark_sql/exercise_43/exercise_43.py
+++ spark_sql/exercise_43/exercise_43.py
@@ -1,12 +1,41 @@
 """
     url: https://jaceklaskowski.github.io/spark-workshop/exercises/spark-sql-exercise-Developing-Custom-Data-Source.html
 """
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
+from pyspark.sql.datasource import DataSource, DataSourceReader
+from pyspark.sql.types import StructType
 
-sc = SparkContext("local", "Simple App")
+class FakeDataSource(DataSource):
+    """
+    A fake data source for PySpark to generate synthetic data using the `faker` library.
+    Options:
+    - numRows: specify number of rows to generate. Default value is 3.
+    """
 
-spark = (SparkSession
-         .builder
-         .appName('exersice_43')
-         .getOrCreate())
+    @classmethod
+    def name(cls):
+        return "fake"
+
+    def schema(self):
+        return "name string, date string, zipcode string, state string"
+
+    def reader(self, schema: StructType):
+        return FakeDataSourceReader(schema, self.options)
+
+    
+class FakeDataSourceReader(DataSourceReader):
+
+    def __init__(self, schema, options):
+        self.schema: StructType = schema
+        self.options = options
+
+    def read(self, partition):
+        from faker import Faker
+        fake = Faker()
+        # Note: every value in this `self.options` dictionary is a string.
+        num_rows = int(self.options.get("numRows", 3))
+        for _ in range(num_rows):
+            row = []
+            for field in self.schema.fields:
+                value = getattr(fake, field.name)()
+                row.append(value)
+            yield tuple(row)
