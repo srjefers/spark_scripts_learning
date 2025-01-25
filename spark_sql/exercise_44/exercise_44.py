afe30566cf18f47d344a961b51b84aceccfff3c7
Jeferson Argueta <ajefersonstiv@gmail.com>
2025-01-24 20:33:50 -0600

    Exercise 44 Dev

---
+++ spark_sql/exercise_44/exercise_44.py
@@ -0,0 +1,16 @@
+"""
+    url: https://jaceklaskowski.github.io/spark-workshop/exercises/spark-sql-exercise-Using-rollup-Operator-for-Total-and-Average-Salaries-by-Department-and-Company-Wide.html
+"""
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+sc = SparkContext("local", "Simple App")
+
+spark = (SparkSession
+         .builder
+         .appName('exersice_44')
+         .getOrCreate())
+
+df = spark.read.options(delimiter=',',inferSchema=True,header=True).csv('data.csv')
+
+df.show()
